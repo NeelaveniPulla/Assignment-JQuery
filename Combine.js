9093df3125f88fe58391fb6eca9da27f3790913d
NeelaveniPulla <81090302+NeelaveniPulla@users.noreply.github.com>
2021-04-04 15:09:18 +0530

    Combine of 2 Strings

---
+++ Combine.js
@@ -0,0 +1,8 @@
+
+function myFunction() {
+  var str1 = "Hello ";
+  var str2 = "world!";
+  var str3 = " Have a nice day!";
+  var res = str1.concat(str2, str3);
+  document.getElementById("demo").innerHTML = res;
+}
